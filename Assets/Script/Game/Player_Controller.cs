a9e5f687b7fd26ca71bb8eaa21eecbb1d6cd4314
SeokWon <boss315@naver.com>
2024-04-08 01:55:27 +0900

    플레이어 이동 구현

--- Assets/Script/Game/Player_Controller.cs
+++ Assets/Script/Game/Player_Controller.cs
@@ -8,8 +8,13 @@ public class Player_Controller : MonoBehaviour{
     public Transform      arrow;
     public float          fixedAngle = 90f;
     public float          angle;
+    public float          angleRotateSpeed;
+    public float          moveSpeed;
 
-    public void Initialize(Direction dir, SpriteRenderer mapSr){
+    private SpriteRenderer mapSr;
+
+    public void Initialize(Direction dir, SpriteRenderer _mapSr){
+        mapSr = _mapSr;
         arrow.gameObject.SetActive(false);
         var tr = transform;
         tr.position = Vector3.zero;
@@ -35,6 +40,53 @@ public class Player_Controller : MonoBehaviour{
         tr.position = startPosition;
     }
 
+    private void OnMouseUp(){
+        StopAllCoroutines();
+        arrow.gameObject.SetActive(false);
+        StartCoroutine(CoMovePlayer());
+    }
+
+    IEnumerator CoMovePlayer(){
+        var tr          = transform;
+        var isLeft      = tr.position.x <= 0;
+        var targetAngle = arrow.rotation.eulerAngles.z;
+        var dirVector   = new Vector3(isLeft ? moveSpeed : moveSpeed * -1, Mathf.Cos(Mathf.Deg2Rad * targetAngle) * moveSpeed, 0);
+
+        var mapBounds    = mapSr.bounds;
+        var playerBounds = sr.bounds;
+        var minVector   = mapBounds.min;
+        var maxVector   = mapBounds.max;
+        minVector.Set(minVector.x + playerBounds.size.x / 2, minVector.y + playerBounds.size.y / 2, 0);
+        maxVector.Set(maxVector.x - playerBounds.size.x / 2, maxVector.y - playerBounds.size.y / 2, 0);
+        
+
+        while (true){
+            tr.position += dirVector;
+
+            if (tr.position.x <= minVector.x){
+                tr.position = new Vector3(minVector.x, tr.position.y);
+                break;
+            }
+            
+            if (tr.position.y <= minVector.y){
+                tr.position = new Vector3(tr.position.x, minVector.y);
+                break;
+            }
+            
+            if (tr.position.x >= maxVector.x){
+                tr.position = new Vector3(maxVector.x, tr.position.y);
+                break;
+            }
+            
+            if (tr.position.y >= maxVector.y){
+                tr.position = new Vector3(tr.position.x, maxVector.y);
+                break;
+            }
+            
+            yield return null;
+        }
+    }
+
     private void OnMouseDown(){
         StopAllCoroutines();
         arrow.gameObject.SetActive(true);
@@ -46,11 +98,38 @@ public class Player_Controller : MonoBehaviour{
         var dotResult   = Vector3.Dot(tr.up,  Vector3.zero - dirPos);
         var resultAngle = dotResult >= 0 ? fixedAngle - targetAngle : fixedAngle + targetAngle;
         
-        tr.Rotate(new Vector3(0, 0, resultAngle));
-        StartCoroutine(CoMoveAngle(targetAngle));
+        StartCoroutine(CoMoveAngle(resultAngle));
     }
 
     IEnumerator CoMoveAngle(float targetAngel){
-        yield return null;
+        var radiAngle  = angle / 2;
+        var startAngle = targetAngel - radiAngle;
+        var endAngle   = targetAngel + radiAngle;
+        var speed      = angleRotateSpeed;
+        
+        arrow.Rotate(new Vector3(0, 0, startAngle));
+
+        while (true){
+            var sumAngel = 0f;
+            while (true){
+                var deltaSpeed = speed;
+                sumAngel += deltaSpeed;
+                arrow.Rotate(new Vector3(0,0, deltaSpeed));
+            
+                if (Mathf.Abs(sumAngel) >= angle){
+                    if (sumAngel < 0){
+                        arrow.rotation = Quaternion.Euler(0,0, startAngle);
+                    }
+                    else{
+                        arrow.rotation = Quaternion.Euler(0,0, endAngle);
+                    }
+                    break;
+                }
+                yield return null;
+            }
+
+            speed *= -1;
+            yield return null;
+        }
     }
 }
