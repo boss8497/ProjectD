cc58b37e32473f92344a96b916cf4be11636d5c2
SeokWon <boss315@naver.com>
2024-04-08 12:23:29 +0900

    버그 수정

--- Assets/Script/Game/Player_Controller.cs
+++ Assets/Script/Game/Player_Controller.cs
@@ -50,54 +50,86 @@ public class Player_Controller : MonoBehaviour{
         var tr          = transform;
         var isLeft      = tr.position.x <= 0;
         var targetAngle = arrow.rotation.eulerAngles.z;
-        var dirVector   = new Vector3(isLeft ? moveSpeed : moveSpeed * -1, Mathf.Cos(Mathf.Deg2Rad * targetAngle) * moveSpeed, 0);
+        targetAngle = isLeft ? fixedAngle + targetAngle : fixedAngle - targetAngle;
+        var speed = isLeft ? moveSpeed : moveSpeed * -1f;
+        var dirVector = new Vector3(Mathf.Cos(Mathf.Deg2Rad * targetAngle) * speed,
+                                    Mathf.Sin(Mathf.Deg2Rad * targetAngle) * moveSpeed, 0);
 
         var mapBounds    = mapSr.bounds;
         var playerBounds = sr.bounds;
-        var minVector   = mapBounds.min;
-        var maxVector   = mapBounds.max;
+        var minVector    = mapBounds.min;
+        var maxVector    = mapBounds.max;
         minVector.Set(minVector.x + playerBounds.size.x / 2, minVector.y + playerBounds.size.y / 2, 0);
         maxVector.Set(maxVector.x - playerBounds.size.x / 2, maxVector.y - playerBounds.size.y / 2, 0);
-        
 
+
+        var resultPos = Vector3.zero;
         while (true){
             tr.position += dirVector;
 
             if (tr.position.x <= minVector.x){
-                tr.position = new Vector3(minVector.x, tr.position.y);
-                break;
-            }
-            
-            if (tr.position.y <= minVector.y){
-                tr.position = new Vector3(tr.position.x, minVector.y);
+                resultPos.x = minVector.x;
+                resultPos.y = tr.position.y;
+                if (tr.position.y <= minVector.y){
+                    resultPos.y = minVector.y;
+                }
+                if (tr.position.y >= maxVector.y){
+                    resultPos.y = maxVector.y;
+                }
                 break;
             }
             
             if (tr.position.x >= maxVector.x){
-                tr.position = new Vector3(maxVector.x, tr.position.y);
+                resultPos.x = maxVector.x;
+                resultPos.y = tr.position.y;
+                if (tr.position.y <= minVector.y){
+                    resultPos.y = minVector.y;
+                }
+                if (tr.position.y >= maxVector.y){
+                    resultPos.y = maxVector.y;
+                }
+                break;
+            }
+
+            if (tr.position.y <= minVector.y){
+                resultPos.y = minVector.y;
+                resultPos.x = tr.position.x;
+                if (tr.position.x <= minVector.x){
+                    resultPos.x = minVector.x;
+                }
+                if (tr.position.x >= maxVector.x){
+                    resultPos.x = maxVector.x;
+                }
                 break;
             }
             
             if (tr.position.y >= maxVector.y){
-                tr.position = new Vector3(tr.position.x, maxVector.y);
+                resultPos.y = maxVector.y;
+                resultPos.x = tr.position.x;
+                if (tr.position.x <= minVector.x){
+                    resultPos.x = minVector.x;
+                }
+                if (tr.position.x >= maxVector.x){
+                    resultPos.x = maxVector.x;
+                }
                 break;
             }
-            
             yield return null;
         }
+        tr.position = resultPos;
     }
 
     private void OnMouseDown(){
         StopAllCoroutines();
         arrow.gameObject.SetActive(true);
         arrow.rotation = new Quaternion();
-        
+
         var tr          = arrow;
         var dirPos      = tr.position;
         var targetAngle = Mathf.Acos(dirPos.x / dirPos.magnitude) * Mathf.Rad2Deg;
-        var dotResult   = Vector3.Dot(tr.up,  Vector3.zero - dirPos);
+        var dotResult   = Vector3.Dot(tr.up, Vector3.zero - dirPos);
         var resultAngle = dotResult >= 0 ? fixedAngle - targetAngle : fixedAngle + targetAngle;
-        
+
         StartCoroutine(CoMoveAngle(resultAngle));
     }
 
@@ -106,7 +138,7 @@ public class Player_Controller : MonoBehaviour{
         var startAngle = targetAngel - radiAngle;
         var endAngle   = targetAngel + radiAngle;
         var speed      = angleRotateSpeed;
-        
+
         arrow.Rotate(new Vector3(0, 0, startAngle));
 
         while (true){
@@ -114,17 +146,19 @@ public class Player_Controller : MonoBehaviour{
             while (true){
                 var deltaSpeed = speed;
                 sumAngel += deltaSpeed;
-                arrow.Rotate(new Vector3(0,0, deltaSpeed));
-            
+                arrow.Rotate(new Vector3(0, 0, deltaSpeed));
+
                 if (Mathf.Abs(sumAngel) >= angle){
                     if (sumAngel < 0){
-                        arrow.rotation = Quaternion.Euler(0,0, startAngle);
+                        arrow.rotation = Quaternion.Euler(0, 0, startAngle);
                     }
                     else{
-                        arrow.rotation = Quaternion.Euler(0,0, endAngle);
+                        arrow.rotation = Quaternion.Euler(0, 0, endAngle);
                     }
+
                     break;
                 }
+
                 yield return null;
             }
 
